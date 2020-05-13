9c37bfba31872e1e31d5bd2e6bf11493486e51a5
im-hungry-for-petrol <64672413+im-hungry-for-petrol@users.noreply.github.com>
2020-05-13 18:17:23 -0400

    Update api_test.py

--- api_test.py
+++ api_test.py
@@ -8,7 +8,7 @@ url = "https://worldwide-restaurants.p.rapidapi.com/search"
 payload = "limit=30&language=en_US&location_id=297704&currency=USD"
 headers = {
     'x-rapidapi-host': "worldwide-restaurants.p.rapidapi.com",
-    'x-rapidapi-key': "bab91b1a89mshb6facec091e50a8p1e6074jsn39ac62f632ad",
+    'x-rapidapi-key': "",
     'content-type': "application/x-www-form-urlencoded"
     }
 
