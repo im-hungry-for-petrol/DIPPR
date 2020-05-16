1a7a1baa106865d06cb456ecc0100da9a9d583db
Jackie <im.hungy.for.petrol@gmail.com>
2020-05-16 15:49:16 -0400

    Finally something that fucking works.

--- app.py
+++ app.py
@@ -1,31 +1,40 @@
+"""
+im-hungry-for-petrol
+app.def
+Main backend for DIPPR
+"""
 from flask import Flask, render_template, request, session # other shit
 from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
-# import Flask-Session
+import func_assist # imported files to making it more organized
 import requests
 import re
 import uuid
+import ast
 
 # basic flask app stuff
 app = Flask(__name__)
 app.secret_key = b'myFUKINGasshurtsBITCH'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dippr_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dippr.db'
+
+def generate_session_id():
+    return str(uuid.uuid1())
 
 # database stuff
 db = SQLAlchemy(app)
-class sessions(db.Model):
+class Joined(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    session_name = db.Column(db.String(15), unique=True, nullable=False)
-    # pickled list of joined members?
-    joined = db.Column(db.String(1500), unique=True,nullable=False)
+    session_name = db.Column(db.String(15), unique=True# never mind this dumb ass file
+, nullable=False)
+    joined = db.Column(db.Text, unique=True,nullable=False)
 
     def __init__(self, session_name, host):
         # creating session name, immediately adding host
         self.session_name = session_name
-        self.joined = []
-        self.joined.append(host)
+        host_list_format = []
+        host_list_format.append(host)
+        self.joined = str(host_list_format)
 
-        session.modified = True
     def __repr__(self):
         return repr('_sessions__: ' + str(self.id) + ' ' + str(self.session_name)
             + ' ' + str(self.joined))
@@ -35,45 +44,38 @@ socketio = SocketIO(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def homepage():
+    user_session_current = generate_session_id()
+
+    # checking for POST request
     if request.method == 'POST':
-        # creating a unique session id
-        session['session_id'] = str(uuid.uuid1())
-        user_session = session['session_id']
-
         if request.form.get('join_submit'):
-            # query db to see if host code exists.
-            sesh_name = request.form['join_code']
-            query = sessions.query.filter_by(session_name=sesh_name).first()
-            # if so, make a connection to session
-            if query is not None:
-                update_session = sessions.query.filter_by(session_name=sesh_name).first()
-                appended_value = update_session.joined
-                appended_value = appended_value.split()
-                appended_value.append(user_session)
-                update_session = sessions.query.filter_by(session_name=sesh_name).first()
-                update_session.joined = str(appended_value)
-                db.session.commit()
-            else:
-                print('''The session does not exist. Please try again after
-                    your host has made a new session. ''')
-                print(query)
+            print("Trying to join.")
+            join_code = request.form['join_code']
+            query = Joined.query.filter_by(session_name=join_code).all()
+            query_list = query[0].joined    # STRING
+            query_list = ast.literal_eval(query_list)
+            query_list.append(user_session_current)
+            curr_host = query_list[0]
 
-        if request.form.get('host_submit'):
-            # check to make sure session code doesn't already exist
-            sesh_name = request.form['host_code']
-            check = sessions.query.filter_by(session_name=sesh_name)
-            # make new session in db, join as host
-            if check is not None:
-                try:
-                    host = sessions(sesh_name, user_session)
-                    db.session.add(host)
-                    db.session.commit()
-                except:
-                    print("For the session: ")
-                    print(session['session_id'])
-                    print("Process could not be completed.")
+            # update
+            update_query = Joined.query.filter_by(session_name=join_code).first()
+            update_query.joined = str(query_list)
+            db.session.commit()
 
-        return render_template('lobby.html', session_id=session['session_id'])
+            print("Successfully joined.")
+            return render_template('lobby.html', session_id=user_session_current, host=curr_host)
+        elif request.form.get('host_submit'):
+            # new session, unique host
+            print("Trying to add new host.")
+            host_code = request.form['host_code']
+            new_group = Joined(session_name=host_code, host=user_session_current)
+            db.session.add(new_group)
+            db.session.commit()
+            print("Done.")
+            return render_template('lobby.html', session_id=user_session_current, host=user_session_current)
+        else:
+            return render_template('index.html', message='Invalid request.')
+
 
     return render_template('index.html')
 
